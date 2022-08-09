dc4f26cd536bdc05349680e41c9474c2d544724d
Miguel Bedoya <me.bedoya10@gmail.com>
2022-08-09 10:15:14 -0500

    Conect the get request to the api pokemons route with the home route in react

--- client/src/components/Pokemons.jsx
+++ client/src/components/Pokemons.jsx
@@ -1,17 +1,36 @@
 import Pokemon from './Pokemon';
 // import { bulbasaur } from '../pokeData';
-import pokemons from '../pokeData';
+// import pokemons from '../pokeData';
+import { connect } from 'react-redux';
 
-function Pokemons() {
+function Pokemons(props) {
   return (
     <div>
       <h1>Pokemons</h1>
       {
-        pokemons.map((poke, index) => <Pokemon name={poke.name} type={poke.typeNames[0]} image={poke.imgUrl} key={index + 1} />)
+        props.pokemons.map((poke, index) => <Pokemon name={poke.name} type={poke.typeNames[0]} image={poke.imgUrl} key={index + 1} />)
       }
+      {/* {
+        pokemons.map((poke, index) => <Pokemon name={poke.name} type={poke.typeNames[0]} image={poke.imgUrl} key={index + 1} />)
+      } */}
       {/* <Pokemon name={bulbasaur.name} type={bulbasaur.typeNames[0]} image={bulbasaur.imgUrl} />  */}
     </div>
   );
 }
 
-export default Pokemons;
+const mapStateToProps = (state) => {
+  return {
+    pokemons: state.pokemonsLoaded
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Pokemons);
+
+// export default Pokemons;
