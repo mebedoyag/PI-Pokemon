2712871701910a444bb633f4824918925c25fa70
Miguel Bedoya <me.bedoya10@gmail.com>
2022-08-17 18:52:07 -0500

    Add styles to the Landing component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom';
 import { getPokemons, getTypes } from '../actions/index';
 import { connect } from 'react-redux';
+import s from './Landing.module.css';
+// import back from '../images/landing-backg2.jpg';
 
 function Landing(props) {
   return (
-    <div>
-      <h1>Henry Pokemon</h1>
-      <Link to="/home">
-        <button onClick={() => {
-          props.getPokemons();
-          props.getTypes();
-          }}>Ingresar</button>
-      </Link>
+    <div className={s.hero}>
+      <div className={s.container}>
+        <h1 className={s.title}>Henry Pokemon</h1>
+        <Link to="/home">
+          <button className={s.bttn} onClick={() => {
+            props.getPokemons();
+            props.getTypes();
+            }}>Ingresar</button>
+        </Link>
+        {/* <div>
+          <img src={back} alt="" />
+        </div> */}
+      </div>
     </div>
   );
 }
