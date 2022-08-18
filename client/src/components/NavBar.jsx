a225f0336c9a45786f90255a548b1e43e83f22f2
Miguel Bedoya <me.bedoya10@gmail.com>
2022-08-17 19:14:06 -0500

    Add styles to NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
+import s from './NavBar.module.css';
+import pokeball from '../images/pokeball.png';
 
 function NavBar() {
   return (
-    <div>
-      <p>LOGO</p>
-      <h1>Pokemon</h1>
-      <ul>
+    <div className={s.container}>
+      <div>
+        <img src={pokeball} alt="" />
+      </div>
+      <h1 className={s.title}>Pokemon</h1>
+      <ul className={s.list}>
         <Link to="/home">
           <li>Home</li>
         </Link>
