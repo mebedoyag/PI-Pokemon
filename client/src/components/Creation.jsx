19280c92ff614b6036a6cb2fe3be344d668e20fb
Miguel Bedoya <me.bedoya10@gmail.com>
2022-08-18 08:58:31 -0500

    Add styles to form

--- client/src/components/Creation.jsx
+++ client/src/components/Creation.jsx
@@ -3,6 +3,7 @@ import validate from '../utils';
 
 import { connect } from 'react-redux';
 import { postPokemons } from '../actions/index';
+import s from './Creation.module.css';
 
 const host = 'http://localhost:3001';
 
@@ -49,51 +50,53 @@ function Creation(props) {
   }
 
   return (
-    <form action={`${host}/pokemons`} method="post" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" onChange={handleInpChan} value={input.name} />
-      </div>
-      <div>
-        {/* <label htmlFor="typeOne">Type</label>
-        <input type="text" name="typeOne" id="typeOne" onChange={handleInpChan} value={input.typeOne} /> */}
-        <label htmlFor="typeOne">Type</label>
-        <select name="typeOne" id="typeOne" onChange={handleInpChan}>
-          {
-            props.types.map(type => <option key={type.id} value={type.id}>{type.name}</option>)
-          }
-        </select>
-      </div>
-      <div>
-        <label htmlFor="height">Height</label>
-        <input type="text" name="height" id="height" onChange={handleInpChan} value={input.height} />
-      </div>
-      <div>
-        <label htmlFor="weight">Weight</label>
-        <input type="text" name="weight" id="weight" onChange={handleInpChan} value={input.weight} />
-      </div>
+    <div className={s.container}>
+      <form className={s.form}  action={`${host}/pokemons`} method="post" onSubmit={handleSubmit}>
+        <div>
+          <label className={s.lab} htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" onChange={handleInpChan} value={input.name} />
+        </div>
+        <div>
+          {/* <label htmlFor="typeOne">Type</label>
+          <input type="text" name="typeOne" id="typeOne" onChange={handleInpChan} value={input.typeOne} /> */}
+          <label htmlFor="typeOne">Type</label>
+          <select name="typeOne" id="typeOne" onChange={handleInpChan}>
+            {
+              props.types.map(type => <option key={type.id} value={type.id}>{type.name}</option>)
+            }
+          </select>
+        </div>
+        <div>
+          <label htmlFor="height">Height</label>
+          <input type="text" name="height" id="height" onChange={handleInpChan} value={input.height} />
+        </div>
+        <div>
+          <label htmlFor="weight">Weight</label>
+          <input type="text" name="weight" id="weight" onChange={handleInpChan} value={input.weight} />
+        </div>
 
-      <div>
-        <label htmlFor="life">Life</label>
-        <input type="text" name="life" id="life" onChange={handleInpChan} value={input.life} />
-      </div>
-      <div>
-        <label htmlFor="attack">Attack</label>
-        <input type="text" name="attack" id="attack" onChange={handleInpChan} value={input.attack} />
-      </div>
-      <div>
-        <label htmlFor="defense">Defense</label>
-        <input type="text" name="defense" id="defense" onChange={handleInpChan} value={input.defense} />
-      </div>
-      <div>
-        <label htmlFor="speed">Speed</label>
-        <input type="text" name="speed" id="speed" onChange={handleInpChan} value={input.speed} />
-      </div>
-      
-      <div>
-        <input type="submit" value="Create" />
-      </div>
-    </form>
+        <div>
+          <label htmlFor="life">Life</label>
+          <input type="text" name="life" id="life" onChange={handleInpChan} value={input.life} />
+        </div>
+        <div>
+          <label htmlFor="attack">Attack</label>
+          <input type="text" name="attack" id="attack" onChange={handleInpChan} value={input.attack} />
+        </div>
+        <div>
+          <label htmlFor="defense">Defense</label>
+          <input type="text" name="defense" id="defense" onChange={handleInpChan} value={input.defense} />
+        </div>
+        <div>
+          <label htmlFor="speed">Speed</label>
+          <input type="text" name="speed" id="speed" onChange={handleInpChan} value={input.speed} />
+        </div>
+        
+        <div className={s.wrapper}>
+          <input className={s.bttn} type="submit" value="Create" />
+        </div>
+      </form>
+    </div>
   );
 }
 
