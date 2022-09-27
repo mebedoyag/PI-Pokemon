baba55649628cc0d44ed6c06a75975da58f039df
Miguel Bedoya <me.bedoya10@gmail.com>
2022-09-27 18:46:50 -0500

    do some superficial changes on Pokemons and Pokemon

--- client/src/components/Pokemon.jsx
+++ client/src/components/Pokemon.jsx
@@ -4,11 +4,15 @@ import { getPokemonDetail } from '../actions/index';
 import s from './Pokemon.module.css';
 
 function Pokemon(props) {
-
   return (
     <div className={s.container}>
       <div>
-        <img src={props.image} width={150} height={150} alt="poke" />
+        <img 
+          src={props.image} 
+          width={150} 
+          height={150} 
+          alt="poke" 
+        />
       </div>
       <Link to={`/detail/${props.id}`}>
         <p>Name: {props.name}</p> 
@@ -19,10 +23,6 @@ function Pokemon(props) {
   );
  }
 
- const mapStateToProps = (state) => {
-  return {};
- };
-
  const mapDispatchToProps = (dispatch) => {
   return {
     getPokemonDetail: (id) => dispatch(getPokemonDetail(id))
@@ -30,6 +30,6 @@ function Pokemon(props) {
  };
 
  export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
  )(Pokemon);
--- client/src/components/Pokemons.jsx
+++ client/src/components/Pokemons.jsx
@@ -1,25 +1,45 @@
 import Pokemon from './Pokemon';
 import { connect } from 'react-redux';
-
 import { changePage } from '../actions/index';
 import s from './Pokemons.module.css';
 
 function Pokemons(props) {
-  const pokeNumber = 12;
+  const pokeNumber = 10;
   const { page, pokemons, changePage } = props;
-  const currentPokemons = props.pokemons.slice(page * pokeNumber, (page + 1) * pokeNumber);
+
+  const startPos = page * pokeNumber;
+  const endPos = (page + 1) * pokeNumber;
+
+  const currentPokemons = () => (
+    pokemons.slice(startPos, endPos)
+  );
 
   return (
     <div className={s.container}>
       <div className={s.pagination}>
-        <button className={s.bttn} onClick={() => page ? changePage(page - 1) : null}>Previous</button>
+        <button 
+          className={s.bttn} 
+          onClick={() => page ? changePage(page - 1) : null}
+        >
+          Previous
+        </button>
         <span className={s.page}>{page + 1}</span>
-        <button className={s.bttn} onClick={() => page < 3 ? changePage(page + 1) : changePage(0)}>Next</button>
+        <button 
+          className={s.bttn} 
+          onClick={() => page < 3 ? changePage(page + 1) : changePage(0)}
+        >
+          Next
+        </button>
       </div>
       <div className={s.wrapper}>
-        {
-          currentPokemons.map((poke, index) => <Pokemon name={poke.name} type={poke.typeNames} image={poke.imgUrl} key={index + 1} id={poke.id} />)
-        }
+        {currentPokemons().map((poke, index) => (
+          <Pokemon 
+            name={poke.name} 
+            type={poke.typeNames} 
+            image={poke.imgUrl} 
+            key={index + 1} 
+            id={poke.id} 
+          />))}
       </div>
     </div>
   );
