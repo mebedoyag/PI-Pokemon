892252ea7d441e42356386f3563395138e437f89
Miguel Bedoya <me.bedoya10@gmail.com>
2022-09-27 08:00:52 -0500

    change the name of PokeSearch to Search

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getPokemonByName } from '../actions/index';
+import { connect } from 'react-redux';
+import s from './Search.module.css';
+
+function Search(props) {
+  const [input, setInput] = useState('');
+
+  return (
+    <div>
+      <input className={s.global} type="text" onChange={(e) => setInput(e.target.value)} value={input} />
+      <Link to={`/detail?name=${input}`}>
+        <button className={`${s.global} ${s.btt}`} onClick={() => props.getPokemonByName(input)}>
+          <span>Le's go</span>
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPokemonByName: name => dispatch(getPokemonByName(name))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Search);
