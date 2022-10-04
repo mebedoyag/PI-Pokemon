fec199e30d75312960b13808e6f6f4c69bb898f9
Miguel Bedoya <me.bedoya10@gmail.com>
2022-10-04 11:46:39 -0500

    add controllers folder and files to handle route logic

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index.js');
+
+// Routes
+const routes = require('./routes/pokemons.routes.js');
 
 require('./db.js'); 
 
