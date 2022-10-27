545f06b2ddab49b50dea27adbb242308dbc0e1ed
Miguel Bedoya <me.bedoya10@gmail.com>
2022-10-27 09:26:04 -0500

    move fetching pokeapi and cleaning localStorage to useEffect in landing component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,19 +1,27 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getPokemons, getTypes } from '../actions/index';
 import { connect } from 'react-redux';
+
 import s from './Landing.module.css';
 
+import { 
+  getPokemons, 
+  getTypes 
+} from '../actions/index';
+
 function Landing(props) {
+  useEffect(() => {
+    localStorage.removeItem('pokes');
+    props.getPokemons();
+    props.getTypes();
+  }, [])
+
   return (
     <div className={s.hero}>
       <div className={s.container}>
         <h1 className={s.title}>Henry Pokemon</h1>
         <Link to="/home">
-          <button className={s.bttn} onClick={() => {
-            localStorage.removeItem('pokes');
-            props.getPokemons();
-            props.getTypes();
-            }}>Ingresar</button>
+          <button className={s.bttn}>Ingresar</button>
         </Link>
       </div>
     </div>
